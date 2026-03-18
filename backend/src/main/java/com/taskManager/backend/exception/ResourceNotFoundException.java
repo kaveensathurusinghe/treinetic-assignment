f2a8a8fdf5e76be9fc73e0f81b9e1a63b79f0979
Kaveen <kaveensathurusinghe@gmail.com>
2026-03-19 00:32:19 +0530

    exception handling

---
+++ backend/src/main/java/com/taskManager/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.taskManager.backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
